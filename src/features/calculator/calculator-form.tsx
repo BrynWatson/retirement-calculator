bd02186d759ad25977dec5b3a8851fa31d380744
Bryn Watson <83179949+BrynWatson@users.noreply.github.com>
2025-08-02 13:37:49 +0200

    Add hook form and abstracted text fields

--- src/features/calculator/calculator-form.tsx
+++ src/features/calculator/calculator-form.tsx
@@ -1,29 +1,48 @@
-import { Box, TextField, Button, Typography, Stack } from "@mui/material";
+import { Box, Button, Typography, Stack } from "@mui/material";
+import { Field, Form } from "../../components/hook-form";
+import { useForm } from "react-hook-form";
 
 export const CalculatorForm = () => {
+  const methods = useForm();
+
+  const { handleSubmit } = methods;
+
+  const onSubmit = handleSubmit(async (data) => {});
   return (
     <Box sx={{ maxWidth: 400, mx: "auto", mt: 4 }}>
       <Typography variant="h4" gutterBottom>
         Retirement Calculator
       </Typography>
+      <Form methods={methods} onSubmit={onSubmit}>
+        <Stack spacing={2}>
+          <Field.Text name="currentAge" label="Current Age" type="number" />
 
-      <Stack spacing={2}>
-        <TextField label="Current Age" type="number" required />
-        <TextField label="Retirement Age" type="number" required />
-        <TextField label="Current Savings" type="number" required />
-        <TextField label="Monthly Contribution" type="number" required />
-        <TextField
-          label="Expected Annual Return"
-          type="number"
-          slotProps={{
-            input: {
-              endAdornment: <span>%</span>,
-            },
-          }}
-          required
-        />
-        <Button variant="contained">Calculate</Button>
-      </Stack>
+          <Field.Text
+            name="retirementAge"
+            label="Retirement Age"
+            type="number"
+          />
+
+          <Field.Text
+            name="currentSavings"
+            label="Current Savings"
+            type="number"
+          />
+
+          <Field.Text
+            name="monthlyContribution"
+            label="Monthly Contribution"
+            type="number"
+          />
+
+          <Field.Text
+            name="annualInterestRate"
+            label="Expected Annual Return (%)"
+            type="number"
+          />
+          <Button variant="contained">Calculate</Button>
+        </Stack>
+      </Form>
     </Box>
   );
 };
